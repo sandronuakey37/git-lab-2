c09abad39f3feecfbe3da37ccb9dd9d4761b59c5
Sandro Nuakey <sn261818@ohio.edu>
2022-01-21 15:39:15 -0500

    Adding the program git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Sandro Nuakey
+ *        Date: 01/21/22
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
